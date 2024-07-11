53380cb504c97c2ce0a66d2c2ff5921c4928f4a7
ramkumarn179 <ramkumarn179@gmail.com>
2024-07-11 15:49:22 +0530

    #

--- Food Order App/backend/src/constants/orderStatus.js
+++
@@ -1,7 +0,0 @@
-export const OrderStatus = {
-  NEW: 'NEW',
-  PAYED: 'PAYED',
-  SHIPPED: 'SHIPPED',
-  CANCELED: 'CANCELED',
-  REFUNDED: 'REFUNDED',
-};
--- Food Order App/frontend/src/constants/patterns.js
+++
@@ -1,4 +0,0 @@
-export const EMAIL = {
-  value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,63}$/i,
-  message: 'Email Is Not Valid',
-};
